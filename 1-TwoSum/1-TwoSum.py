05fd8a73225503fdeecaa0084c101da6307825ae
MUHAMMAD SHAYAN AKRAM <161307847+shayancodi@users.noreply.github.com>
2026-08-08 02:07:16 +0500

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,10 @@
+# Last updated: 8/8/2026, 2:07:16 AM
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
